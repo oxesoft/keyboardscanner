ce948a8bf6e6fc78fca4ceb45c1d16e00d7df026
Emerson Seiler <seiler.emerson@gmail.com>
2025-10-15 16:39:07 +0000

    style: Apply 4-space indentation across the codebase

--- kuzweil_sp76ii/code_tabs/input.h
+++ kuzweil_sp76ii/code_tabs/input.h
@@ -1,29 +1,33 @@
-void readInputs() {
-  // byte pedal = LOW;
-  if (pedal_enabled) {
-    pedal = digitalRead2(PEDAL_PIN);
-  }
+void readInputs()
+{
+    // byte pedal = LOW;
+    if (pedal_enabled)
+    {
+        pedal = digitalRead2(PEDAL_PIN);
+    }
 
-  boolean *s = signals;
-  for (byte i = 0; i < KEYS_NUMBER * 2; i++) {
-    byte output_pin = output_pins[i];
-    byte input_pin = input_pins[i];
-    digitalWrite2(output_pin, LOW);
-    *(s++) = !digitalRead2(input_pin);
-    digitalWrite2(output_pin, HIGH);
-  }
+    boolean *s = signals;
+    for (byte i = 0; i < KEYS_NUMBER * 2; i++)
+    {
+        byte output_pin = output_pins[i];
+        byte input_pin = input_pins[i];
+        digitalWrite2(output_pin, LOW);
+        *(s++) = !digitalRead2(input_pin);
+        digitalWrite2(output_pin, HIGH);
+    }
 }
 
-
-void countCicles() {
-  static unsigned long cycles = 0;
-  static unsigned long start = 0;
-  static unsigned long current = 0;
-  cycles++;
-  current = millis();
-  if (current - start >= 1000) {
-    Serial.println(cycles);
-    cycles = 0;
-    start = current;
-  }
+void countCicles()
+{
+    static unsigned long cycles = 0;
+    static unsigned long start = 0;
+    static unsigned long current = 0;
+    cycles++;
+    current = millis();
+    if (current - start >= 1000)
+    {
+        Serial.println(cycles);
+        cycles = 0;
+        start = current;
+    }
 }
--- kuzweil_sp76ii/code_tabs/states.h
+++ kuzweil_sp76ii/code_tabs/states.h
@@ -1,68 +1,82 @@
-void updateStates() {
-  byte *state = keys_state;
-  unsigned long *ktime = keys_time;
-  boolean *signal = signals;
-  for (byte key = 0; key < KEYS_NUMBER; key++) {
-    for (byte state_index = 0; state_index < 2; state_index++) {
-      switch (*state) {
-        case KEY_OFF:
-          if (state_index == 0 && *signal) {
-            *state = KEY_START;
-            *ktime = millis();
-          }
-          break;
-        case KEY_START:
-          if (state_index == 0 && !*signal) {
-            *state = KEY_OFF;
-            break;
-          }
-          if (state_index == 1 && *signal) {
-            *state = KEY_ON;
-            send_midi_event(0x90, key, millis() - *ktime);
-          }
-          break;
-        case KEY_ON:
-          if (state_index == 1 && !*signal) {
-            *state = KEY_RELEASED;
-            *ktime = millis();
-          }
-          break;
-        case KEY_RELEASED:
-          if (state_index == 0 && !*signal) {
-            if (pedal) {
-              *state = KEY_SUSTAINED;
-              break;
+void updateStates()
+{
+    byte *state = keys_state;
+    unsigned long *ktime = keys_time;
+    boolean *signal = signals;
+    for (byte key = 0; key < KEYS_NUMBER; key++)
+    {
+        for (byte state_index = 0; state_index < 2; state_index++)
+        {
+            switch (*state)
+            {
+            case KEY_OFF:
+                if (state_index == 0 && *signal)
+                {
+                    *state = KEY_START;
+                    *ktime = millis();
+                }
+                break;
+            case KEY_START:
+                if (state_index == 0 && !*signal)
+                {
+                    *state = KEY_OFF;
+                    break;
+                }
+                if (state_index == 1 && *signal)
+                {
+                    *state = KEY_ON;
+                    send_midi_event(0x90, key, millis() - *ktime);
+                }
+                break;
+            case KEY_ON:
+                if (state_index == 1 && !*signal)
+                {
+                    *state = KEY_RELEASED;
+                    *ktime = millis();
+                }
+                break;
+            case KEY_RELEASED:
+                if (state_index == 0 && !*signal)
+                {
+                    if (pedal)
+                    {
+                        *state = KEY_SUSTAINED;
+                        break;
+                    }
+                    *state = KEY_OFF;
+                    send_midi_event(0x80, key, millis() - *ktime);
+                }
+                break;
+            case KEY_SUSTAINED:
+                if (!pedal)
+                {
+                    *state = KEY_OFF;
+                    send_midi_event(0x80, key, MAX_TIME_MS);
+                }
+                if (state_index == 0 && *signal)
+                {
+                    *state = KEY_SUSTAINED_RESTART;
+                    *ktime = millis();
+                }
+                break;
+            case KEY_SUSTAINED_RESTART:
+                if (state_index == 0 && !*signal)
+                {
+                    *state = KEY_SUSTAINED;
+                    digitalWrite(13, HIGH);
+                    break;
+                }
+                if (state_index == 1 && *signal)
+                {
+                    *state = KEY_ON;
+                    send_midi_event(0x80, key, MAX_TIME_MS);
+                    send_midi_event(0x90, key, millis() - *ktime);
+                }
+                break;
             }
-            *state = KEY_OFF;
-            send_midi_event(0x80, key, millis() - *ktime);
-          }
-          break;
-        case KEY_SUSTAINED:
-          if (!pedal) {
-            *state = KEY_OFF;
-            send_midi_event(0x80, key, MAX_TIME_MS);
-          }
-          if (state_index == 0 && *signal) {
-            *state = KEY_SUSTAINED_RESTART;
-            *ktime = millis();
-          }
-          break;
-        case KEY_SUSTAINED_RESTART:
-          if (state_index == 0 && !*signal) {
-            *state = KEY_SUSTAINED;
-            digitalWrite(13, HIGH);
-            break;
-          }
-          if (state_index == 1 && *signal) {
-            *state = KEY_ON;
-            send_midi_event(0x80, key, MAX_TIME_MS);
-            send_midi_event(0x90, key, millis() - *ktime);
-          }
-          break;
-      }
-      signal++;
+            signal++;
+        }
+        state++;
+        ktime++;
     }
-    state++;
-    ktime++;
-  }
 }
